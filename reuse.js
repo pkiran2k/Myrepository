3406c71d092da55c0d0d5644457e7db185f05a2a
Kiran Kumar Paithali <pkiran2k@yahoo.com>
2023-02-22 04:24:34 +0000

    my code

---
+++ reuse.js
@@ -0,0 +1,38 @@
+// module.exports = {
+
+//     printf: function(arrObject){
+//         for (let i = 0; i < arrObject.length; i++) {
+//             const element = arrObject[i];
+//             console.log(element);
+//         }
+//     },
+
+//     getCount: function(arr){
+//         console.log(arr.length);
+//     },
+
+//     removeItem(arr, index){
+//         return arr.splice(index, 1);
+//     }
+
+
+// }
+module.exports = {
+
+    printf: function(arrObject){
+        for (let i = 0; i < arrObject.length; i++) {
+            const element = arrObject[i];
+            console.log(element);
+        }
+    },
+
+    getCount: function(arr){
+        console.log(arr.length);
+    },
+
+    removeItem(arr, index){
+        return arr.splice(index, 1);
+    }
+
+
+}
